f92175f42152bc7890e536fdc1422e20cf0925e2
sumancfc <sumanstha999@gmail.com>
2021-01-07 21:07:07 +0545

    Data fetch on Featured Listing page

--- estate/settings.py
+++ estate/settings.py
@@ -39,9 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'pages',
     'listings',
-    'realtors',
+    'realtors',  
 ]
 
 MIDDLEWARE = [
